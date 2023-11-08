d95339606050d3a06aee7d8d87d931957cc89eef
victoriapavlova <victoria.pavlova7@gmail.com>
2023-11-08 11:39:51 +0300

    Change task title

--- App.tsx
+++ App.tsx
@@ -21,6 +21,9 @@ export default function App() {
     const changeStatus = (id: number, status: boolean) => {
         setTasks(tasks.map(t => t.id === id ? {...t, isDone: status} : t))
     }
+    const changeTask = (id: number, title: string) => {
+        setTasks(tasks.map(t => t.id === id ? {...t, title} : t))
+    }
     const deleteTask = (id: number) => {
         setTasks(tasks.filter(t => t.id !== id))
     }
@@ -28,20 +31,21 @@ export default function App() {
         <View style={styles.container}>
             <Text style={{color: 'white'}}>Add new task</Text>
             <HideKeyboard>
-                    <View style={{width: '60%'}}>
-                        <TextInput style={[globalStyles.border, styles.input]} value={value} onChangeText={setValue}/>
-                    </View>
+                <View style={{width: '60%'}}>
+                    <TextInput style={[globalStyles.border, styles.input]} value={value} onChangeText={setValue}/>
+                </View>
             </HideKeyboard>
-            <Button title={'Add task'} onPress={addTask}  />
+            <Button title={'Add task'} onPress={addTask}/>
 
             <View style={{width: '60%'}}>
                 {tasks.map((t) => {
                     return <View key={t.id} style={[styles.boxTask]}>
-                        <Checkbox value={t.isDone} onValueChange={(value)=>changeStatus(t.id, value)}/>
+                        <Checkbox value={t.isDone} onValueChange={(value) => changeStatus(t.id, value)}/>
                         {show === t.id
-                            ? <Text onPress={()=>setShow(null )}>Change</Text>
-                            : <Text onPress={()=>setShow(t.id)}>{t.title}</Text>}
-                        <Button title={'X'} onPress={()=>deleteTask(t.id)}/>
+                            ? <TextInput style={[globalStyles.border, styles.input]} value={t.title}
+                                         onChangeText={(title) => changeTask(t.id, title)}/>
+                            : <Text onPress={() => setShow(t.id)}>{t.title}</Text>}
+                        <Button title={'X'} onPress={() => deleteTask(t.id)}/>
                     </View>
                 })}
             </View>
@@ -49,7 +53,9 @@ export default function App() {
     );
 }
 
-const HideKeyboard = ({ children }: { children: ReactNode }): ReactElement => (
+const HideKeyboard = ({children}: {
+    children: ReactNode
+}): ReactElement => (
     <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
         {children}
     </TouchableWithoutFeedback>
